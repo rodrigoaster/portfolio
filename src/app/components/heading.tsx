0d67c83e655ed7f5bd66eed52c6697fecdfb16d5
Rodrigo Aster <rodrigowebx@gmail.com>
2025-01-17 21:35:31 -0300

    feat(#00002): adjust icon w and h

--- src/app/components/heading.tsx
+++ src/app/components/heading.tsx
@@ -5,7 +5,7 @@ export default function HeadingComponent() {
         <header className="flex justify-center py-8 max-w-[64rem]">
             <nav className="flex flex-1 items-center justify-between mx-auto">
                 <a href="/">
-                    <Image className="hover:opacity-50 ease-in-out duration-300" src="/aster-icon.png" alt="Aster developer icon" width={70} height={70} />
+                    <Image className="hover:opacity-50 ease-in-out duration-300" src="/aster-icon.png" alt="Aster developer icon" width={60} height={65} />
                 </a>
                 <div>
                     <a className="pr-12 text-xl hover:text-gray-500 ease-in-out duration-300" href="/">Home</a>
